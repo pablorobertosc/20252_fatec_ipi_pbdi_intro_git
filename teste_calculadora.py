2995bef445ed044cd399e5cb75cb220cc26560dd
Pablo Roberto <pabloroberto.s.c@gmail.com>
2025-08-19 10:33:23 -0300

    feat(ap01-git): implementa a divisao e mult

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -7,3 +7,9 @@ print(f'{a} + {b} = {soma}')
 
 subtrair = calculadora.subtrair(a, b)
 print(f'{a} + {b} = {subtrair}')
+
+divisão = calculadora.dividir(a, b)
+print(f'{a} + {b} = {divisão}')
+
+multiplicação = calculadora.multiplicar(a, b)
+print(f'{a} + {b} = {multiplicação}')
